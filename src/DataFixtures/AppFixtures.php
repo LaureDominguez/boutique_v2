28b98ce58146d81f1cad3f042b0d49ebc34a9c52
Laure <dominguezlaure@gmail.com>
2022-10-17 10:14:16 +0200

    save changes before switching branch

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,12 +5,28 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use App\Entity\Product;
+use Faker\Factory;
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+
+        for ($i=0; $i<10; $i++){
+            $product = new Product();
+            $product
+            ->setCategory(null)
+            ->setName($faker->word())
+            ->setDescription($faker->realText($faker->numberBetween(25, 100)))
+            ->setPrice($faker->randomDigitNotNull());
+
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
