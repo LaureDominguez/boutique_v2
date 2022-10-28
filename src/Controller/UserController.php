b04716ec6b9139581a9874ae890e4c3b7154d99c
Laure <dominguezlaure@gmail.com>
2022-10-28 12:55:55 +0200

    changed some forms type, added password edit option with password authorisation, working on password hide/show option

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Form\UserEditType;
+use App\Form\PasswordEditType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +14,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
 use Symfony\Component\Routing\Annotation\Route;
 //added "UserPasswordHasherInterface" path
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Controller\SecurityController;
 
 class UserController extends AbstractController
 {
@@ -52,8 +54,8 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/registred/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    #[Route('/registred/{id}/editProfil', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function userEdit(Request $request, User $user, UserRepository $userRepository): Response
     {
         $form = $this->createForm(UserEditType::class, $user);
         $form->handleRequest($request);
@@ -70,13 +72,33 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/user/{id}', name: 'app_user_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    #[Route('/registred/{id}/editPass', name: 'app_password_edit', methods: ['GET', 'POST'])]
+    public function passEdit(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(PasswordEditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit-pass.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+    
+// Bloqué, à fixer
+    #[Route('/registred/{id}', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, UserRepository $userRepository, SecurityController $securityController): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $this->redirectToRoute('app_logout' . $securityController, ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
             $userRepository->remove($user, true);
         }
 
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/PasswordEditType.php
+++ src/Form/PasswordEditType.php
@@ -4,45 +4,34 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-//added "PasswordType" path
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use App\Form\ShowHidePasswordType; // add
 
-class UserType extends AbstractType
+class PasswordEditType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'attr' => [
-                    'class'     => 'form-control',
-                    'minlenght' => '5',
-                    'maxlenght' => '100'
-                ],
-                'label' => 'Email',
-                'label_attr' => [
-                    'class' => 'form-label mb-2'
-                ]
-            ])
-            //->add('roles')
-            //added "PasswordType" class
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 // 'type' => ShowHidePasswordType::class,
-                "attr" => [ "class" => "form-control" ],
+                "attr" => ["class" => "form-control"],
                 'invalid_message' => 'Les mots de passe doivent correspondre.',
-                'options' => ['attr' => ['class' => 'password-field form-control']],
+                'options' => ['attr' => [
+                    'class' => 'password-field form-control',
+                    'value'  => "",
+                    ]],
                 'required' => true,
                 'first_options'  => [
-                    'label' => 'Mot de passe',
+                    'label' => 'Nouveau mot de passe',
                     "label_attr" => ["class" => "form-label"]
                 ],
                 'second_options' => [
-                    'label' => 'Retapez le mot de passe',
+                    'label' => 'Retapez le nouveau mot de passe',
                     "label_attr" => ["class" => "form-label"]
                 ],
                 "label_attr" => ["class" => "form-label"]
